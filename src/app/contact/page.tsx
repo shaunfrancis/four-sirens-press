84f2a63bc2e4403257ad517120197cd4faa3151e
shaunfrancis <shaunfrancis@gmx.com>
2023-11-16 13:10:51 +0000

    Improvements for mobile

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from 'next';
+
 import ContactCard from '@/components/contact-card';
+
+export const metadata : Metadata = {
+    title: 'Contact Us | Four Sirens Press'
+}
 
 export default function Contact(){
     return (
